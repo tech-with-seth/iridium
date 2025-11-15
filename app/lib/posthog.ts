5b3bcb8ffc320b1340cee987458672ba68e9b991
Seth Davis <sethdavis512@gmail.com>
2025-11-14 19:36:43 -0600

    feat: add logging for PostHog API key and environment variables during initialization

--- app/lib/posthog.ts
+++ app/lib/posthog.ts
@@ -1,6 +1,13 @@
 import { PostHog } from 'posthog-node';
 import 'dotenv/config';
 
+console.log(
+    '\n\n',
+    '===== POSTHOG_API_KEY LOG =====',
+    JSON.stringify({ key: process.env.POSTHOG_API_KEY }, null, 4),
+    '\n\n',
+);
+
 export const postHogClient = new PostHog(process.env.POSTHOG_API_KEY!, {
     host: process.env.POSTHOG_HOST,
 });
