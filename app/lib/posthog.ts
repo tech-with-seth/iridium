323e0f50195ec30680a63a8fe388f91b5e0d9d4f
Seth Davis <sethdavis512@gmail.com>
2025-11-15 17:44:16 -0600

    chore: add @posthog/ai dependency to package.json

--- app/lib/posthog.ts
+++ app/lib/posthog.ts
@@ -2,4 +2,6 @@ import { PostHog } from 'posthog-node';
 
 export const postHogClient = new PostHog(process.env.POSTHOG_API_KEY!, {
     host: process.env.POSTHOG_HOST,
+    flushAt: 1,
+    flushInterval: 0,
 });
