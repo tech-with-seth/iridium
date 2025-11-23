f622f81a4f70732d1581546881e121a8e3ca4641
Seth Davis <sethdavis512@gmail.com>
2025-11-22 23:28:33 -0600

    refactor: streamline Prisma client initialization and fix import order in db.server.ts and prisma.config.ts

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,20 +1,11 @@
-import { PrismaClient } from './generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/prisma/client';
 
-const adapter = new PrismaPg({
-    connectionString: process.env.DATABASE_URL!,
-});
+const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+const prisma = new PrismaClient({ adapter: pool });
 
-const globalForPrisma = global as unknown as {
-    prisma: PrismaClient;
-};
-
-const prisma =
-    globalForPrisma.prisma ||
-    new PrismaClient({
-        adapter,
-    });
+const globalForPrisma = global as unknown as { prisma: typeof prisma };
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
-export default prisma;
+export { prisma };
