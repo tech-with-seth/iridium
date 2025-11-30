757fc2a8ec8601506b7094d6673f6c62e92c1a3c
Seth Davis <sethdavis512@gmail.com>
2025-11-29 22:11:36 -0600

    fix: correct successUrl in checkout loader and enhance detail loader with user session

--- app/routes/shop/checkout.tsx
+++ app/routes/shop/checkout.tsx
@@ -2,6 +2,6 @@ import { Checkout } from '@polar-sh/remix';
 
 export const loader = Checkout({
     accessToken: process.env.POLAR_ACCESS_TOKEN,
-    successUrl: process.env.SUCCESS_URL,
+    successUrl: process.env.POLAR_SUCCESS_URL,
     server: process.env.POLAR_SERVER,
 });
